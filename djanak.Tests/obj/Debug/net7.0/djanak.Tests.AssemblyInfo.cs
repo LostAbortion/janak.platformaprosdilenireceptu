0a20bed3e501001dffa6ec2e6a9ddb214b62efef
LostAbortion <d_janak@utb.cz>
2023-12-12 21:42:15 +0100

    Poslední commit1 před mergem s 11tyden_pridavek

--- djanak.Tests/obj/Debug/net7.0/djanak.Tests.AssemblyInfo.cs
+++ djanak.Tests/obj/Debug/net7.0/djanak.Tests.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("djanak.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a6f3bbcab2fdab2ac0cf693f25b578c214437e")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc6a8336b788c8e909139bbd1b883a315834117")]
->>>>>>> 10tyden_pridavek
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("djanak.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("djanak.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
